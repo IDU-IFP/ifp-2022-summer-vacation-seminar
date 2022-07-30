161ad2495288863bbe21d8697c192c3526f9e517
Yongkeun <xdkeun@gmail.com>
2022-07-31 06:44:51 +0900

    feat: 클릭한 카드 내용이 나오도록 수정하였나 차후 추가 수정 필요

--- yongkeun/week3/과제4/app.js
+++ yongkeun/week3/과제4/app.js
@@ -1,8 +1,17 @@
-const backCard = document.querySelector(".back");
-const frontCard = document.querySelector(".front h1:first-child");
+const backCard = document.querySelectorAll(".back");
+const frontCard = document.querySelectorAll(".front");
 
-function clickCard(){
-    alert(frontCard.innerText);
+function firstCardClick(){
+    alert(frontCard[0].innerText);
 }
+backCard[0].addEventListener("click", firstCardClick);
 
-backCard.addEventListener("click", clickCard);
+function secondCardClick(){
+    alert(frontCard[1].innerText);
+}
+backCard[1].addEventListener("click", secondCardClick);
+
+function thirdCardClick(){
+    alert(frontCard[2].innerText);
+}
+backCard[2].addEventListener("click", thirdCardClick);
